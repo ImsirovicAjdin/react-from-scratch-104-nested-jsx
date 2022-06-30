36721ba3795531aca4d79402dfc1bd1947904455
Ajdin Imsirovic <ajdika@live.com>
2022-06-30 10:57:12 +0200

    (3) Extract the JSX elements from App into Heading

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import React from 'react';
+import Headings from "./Headings";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      <h2>Hello again!</h2>
-      <h3>Hello yet again!</h3>
-      <h4>Once again!</h4>
+      <Headings />
     </div>
-    // React.createElement('div', { className: 'App' },
-    //   React.createElement('h1', null, 'Hello World!'),
-    //   React.createElement('h2', null, 'Hello again!'),
-    //   React.createElement('h3', null, 'Hello yet again!'),
-    //   React.createElement('h4', null, 'Once again!'),
   )
 }
 
---
+++ src/Headings.js
@@ -0,0 +1,12 @@
+function Headings() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <h2>Hello again!</h2>
+        <h3>Hello yet again!</h3>
+        <h4>Once again!</h4>
+      </div>
+    )
+  }
+
+export default Headings;
